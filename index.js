375f1a151f4b04fd11f40b554e2b4106584cb407
toukirahmeds <toukirahmeds@gmail.com>
2026-08-12 01:42:30 +0600

    add validations for input

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 import minimist from "minimist";
 
 const {
-    p: pageUrl,
-    s: timeSeconds = 5,
-    m: message = "The target page has been updated!"
+    p,
+    s = 5,
+    m = "The target page has been updated!"
 } = minimist(process.argv);
 
 console.log(minimist(process.argv));
-if (!pageUrl || typeof pageUrl !== "string") {
-    console.error(`The value for '-p' must be provided a valid url.`);
-    process.exit(1);
+let pageUrl;
+
+try {
+    pageUrl = new URL(p);
+} catch (err) {
+    console.error(err);
+    console.error(`Option '-p' must be valid url`);
+    process.exit(9);
 }
 
-console.log(pageUrl, timeSeconds, message)
+let timeSeconds;
+
+try {
+    timeSeconds = parseInt(s);
+
+    if (isNaN(timeSeconds) || timeSeconds < 0) {
+        throw new Error("Must be a valid positive number.");
+    }
+} catch (err) {
+    console.error(err);
+    console.error(`Option '-s' must be a valid positive number.`);
+    process.exit(9);
+}
+
+let message;
+
+try {
+    message = m;
+
+    if (!message || message?.length === 0 || typeof message !== "string") {
+        throw new Error("Must not be a empty string.")
+    }
+} catch (err) {
+    console.error(err);
+    console.error(`Option '-m' must be a valid message string.`);
+    process.exit(9);
+}
+
+console.log(message)
