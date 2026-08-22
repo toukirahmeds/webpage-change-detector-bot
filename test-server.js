b1b6bbe0c8a3b5dd630e21453c9e5fcdd8398a3e
toukirahmeds <toukirahmeds@gmail.com>
2026-08-22 19:34:03 +0600

    add test server

---
+++ test-server.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+
+const { SERVER_PORT = 3000 } = process.env;
+
+const ADDRESS = `http://localhost:${SERVER_PORT}`;
+
+const app = express();
+
+app.use(express.static("public"))
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running on ${ADDRESS}`);
+});
